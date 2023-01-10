2cd7ed72de77a361d86a2601caa1c40bc7cd8997
Arwin-Yu <102544244+Arwin-Yu@users.noreply.github.com>
2023-01-10 20:03:58 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,7 @@
 from rdkit import Chem
-from rdkit.Chem.Draw import MolToImage
-import mlflow
-import deepchem as dc
-import requests
-import torch
-import random
-import numpy as np
-import json
-import time
+from rdkit.Chem.Draw import MolToImage 
+import deepchem as dc 
+import torch 
 
 mlflow.set_tracking_uri("http://localhost:5000")
 
@@ -42,51 +36,7 @@ def mol_to_tensor_graph(mol):
     data["batch_index"] = torch.ones_like(data["x"][:, 0])
     return data
 
-
-def get_model_predictions(payload):
-    """
-    Get model predictions  
-    ENDPOINT = Calls an endpoint to get the predictions
-    REGISTRY = Loads model from registry and predicts
-    MOCKED = Randomly generated prediction
-    """
-    option="MOCKED"
-
-    if option == "ENDPOINT":
-        # Currently not supported for multi-input models
-        DEPLOYED_ENDPOINT = "http://127.0.0.1:5001/invocations"
-        headers = {"Content-Type":"application/json"}
-        prediction = requests.post(url=DEPLOYED_ENDPOINT, 
-                                   data={"inputs": {
-                                            "x": payload["x"].numpy(),
-                                            "edge_attr": payload["edge_attr"].numpy(),
-                                            "edge_index": payload["edge_index"].numpy().astype(np.int32),
-                                            "batch_index": np.expand_dims(payload["batch_index"].numpy().astype(np.int32), axis=1)
-                                        }}, headers=headers)
-        prediction = json.loads(prediction.content.decode("utf-8")) 
-    
-    if option == "REGISTRY":
-        # Currently not supported for multi-input models
-        model_name = "GraphTransformer"
-        model_version = "2"
-        model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_version}")
-
-
-        prediction = model.predict({
-            "x": payload["x"].numpy(),
-            "edge_attr": payload["edge_attr"].numpy(),
-            "edge_index": payload["edge_index"].numpy().astype(np.int32),
-            "batch_index": np.expand_dims(payload["batch_index"].numpy().astype(np.int32), axis=1)
-        })
-        
-    if option == "MOCKED":
-        # Fake API call
-        time.sleep(2)
-        prediction = random.choice([0,1])
-
-    return prediction
-
-    
+ 
 
 
 
